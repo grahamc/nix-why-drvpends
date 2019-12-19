e817c5bdebb883dfded635887dc81e5c37ea0609
Graham Christensen <graham@grahamc.com>
2019-12-19 11:23:19 -0500

    why-drvpends: init

--- shell.nix
+++ shell.nix
@@ -1,8 +1,22 @@
 let
   pkgs = import <nixpkgs> {};
+
+  testrun = pkgs.writeScriptBin "why-drvpends" ''
+    set -eux
+
+    git ls-files '*.nix' | xargs nixpkgs-fmt
+
+    git ls-files '*.py' | xargs mypy
+    git ls-files '*.py' | xargs black
+
+    ./why-drvpends.py "$@"
+  '';
 in
-  pkgs.mkShell {
-    buildInputs = [
-        pkgs.python3
-    ];
-  }
+pkgs.mkShell {
+  buildInputs = [
+    testrun
+    pkgs.nixpkgs-fmt
+    pkgs.mypy
+    (pkgs.python3.withPackages (pypkgs: with pypkgs; [ black ]))
+  ];
+}
---
+++ why-drvpends.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import json
+import argparse
+from pprint import pprint
+from functools import lru_cache
+from typing import List, Union, Any, Generator
+import subprocess
+
+
+def debug(msg: str):
+    # print(msg)
+    pass
+
+
+req_calls: int = 0
+
+
+@lru_cache(maxsize=4096)
+def requisites(drv: str) -> List[str]:
+    global req_calls
+    req_calls += 1
+    debug(f"requisite_calls: {req_calls}")
+
+    proc = subprocess.run(
+        ["nix", "show-derivation", drv], capture_output=True, check=True
+    )
+    result = json.loads(proc.stdout)[drv]
+
+    return list(result["inputDrvs"].keys())
+
+
+spelunk_calls: int = 0
+
+
+@lru_cache(maxsize=4096)
+def spelunk(package_drv: str, dependency_drv: str) -> Generator[List[str], None, None]:
+    global spelunk_calls
+    spelunk_calls += 1
+    debug(f"spelunk_calls: {spelunk_calls}")
+
+    debug(f"{package_drv} -> {dependency_drv}")
+
+    for path in requisites(package_drv):
+        if path == dependency_drv:
+            yield [path]
+        else:
+            for p in spelunk(path, dependency_drv):
+                yield [package_drv] + p
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("package_drv")
+    parser.add_argument("dependency_drv")
+    args = parser.parse_args()
+
+    for route in spelunk(args.package_drv, args.dependency_drv):
+        level = -1
+        for step in route:
+            level += 1
+            print("{indent}=> {step}".format(indent="  " * level, step=step))
+        print()
+
+
+if __name__ == "__main__":
+    main()
